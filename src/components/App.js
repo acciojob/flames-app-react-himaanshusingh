64999c86e00bd0f476625d40b7a6bb6a6651a548
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-25 21:19:13 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,81 +1,58 @@
-import React, { Component, useState } from "react";
-import "../styles/App.css";
+import React, { useState } from "react";
 
 const App = () => {
-  const [name1, setName1] = useState("");
-  const [name2, setName2] = useState("");
-  let arr = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"];
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
   const [ans, setAns] = useState("");
+
+  const values = [
+    "Siblings",
+    "Friends",
+    "Love",
+    "Affection",
+    "Marriage",
+    "Enemy",
+  ];
+
+  function handleChange1(e) {
+    let val = e.target.value;
+    setInput1(val);
+  }
+
+  function handleChange2(e) {
+    let val = e.target.value;
+    setInput2(val);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let arr1 = input1.trim().split("");
+    let arr2 = input2.trim().split("");
+    if (arr1.length == 0 || arr2.length == 0) {
+      setAns("Please Enter valid input");
+      return;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr2.includes(arr1[i])) {
+        arr2[arr2.indexOf(arr1[i])] = "";
+        arr1[i] = "";
+      }
+    }
+    let input1Len = arr1.join("").trim().length;
+    let input2Len = arr2.join("").trim().length;
+    let ansIndex = input1Len + (input2Len % 6);
+    setAns(values[ansIndex]);
+  }
+
   return (
-    <div id="main">
-      <input
-        value={name1}
-        onChange={(e) => {
-          setName1(e.target.value);
-        }}
-        data-testid="input1"
-        name="name1"
-      />
-
-      <input
-        value={name2}
-        onChange={(e) => {
-          setName2(e.target.value);
-        }}
-        data-testid="input2"
-        name="name2"
-      />
-
-      <button
-        onClick={() => {
-          let map = new Map();
-          for (let i = 0; i < name1.length; i++) {
-            if (map.has(name1[i])) {
-              map.set(name1[i], map.get(name1[i]) + 1);
-            } else {
-              map.set(name1[i], 1);
-            }
-          }
-
-          for (let i = 0; i < name2.length; i++) {
-            if (map.has(name2[i])) {
-              map.set(name2[i], map.get(name2[i]) - 1);
-              if (map.get(name2[i]) == 0) {
-                map.delete(name2[i]);
-              }
-            } else {
-              map.set(name2[i], 1);
-            }
-          }
-          let temp = map.values();
-          let sum = 0;
-          for (let item of temp) {
-            sum += item;
-          }
-          sum %= 6;
-          setAns(arr[sum]);
-        }}
-        data-testid="calculate_relationship"
-        name="calculate_relationship"
-      >
-        Calculate Relationship Future
-      </button>
-
-      <button
-        onClick={() => {
-          setName1("");
-          setName2("");
-          setAns("");
-        }}
-        data-testid="clear"
-        name="clear"
-      >
-        Clear
-      </button>
-
-      <h3 data-testid="answer">{ans}</h3>
-    </div>
-  );
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Enter first name" data-testid="input1" name="name1" onChange={handleChange1}/>
+      <input type="text" placeholder="Enter second name" data-testid="input2" name="name2" onChange={handleChange2}/>
+      <button data-testid="calculate_relationship" name="calculate_relationship" type="submit">Calculate Relationship Future</button>
+      <button data-testid="clear" name="clear" type="reset">Clear</button>
+      {{ans} && <h3 data-tesid="answer">{ans}</h3>}
+    </form>
+  ); // prettier-ignore
 };
 
 export default App;
